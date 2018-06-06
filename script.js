27bd9a66299744d382b0558bfe3caaca7018258b
mesuara <mesuara@gmail.com>
2018-06-06 12:11:37 -0400

    show elements in webpage

--- script.js
+++ script.js
@@ -19,14 +19,17 @@ color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 
 o = ["th", "st", "nd", "rd"];
 
+var show = ''
 var count = 1;
 for(var i = 0; i < color.length; i++){
 if(count <= 3){
-console.log(count + o[count] + " choice is " + color[i]);
-count++;
+show += count + o[count] + " choice is " + color[i] + "<br>";
+count++
 }
 else{
-console.log(count + o[0] + " choice is " + color[i])
-count++;
+show += count + o[0] + " choice is " + color[i] + "<br> ";
+count++
 }
 }
+
+document.getElementById('hello').innerHTML = show
